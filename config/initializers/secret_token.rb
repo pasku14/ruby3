81b432eb44a6d3ed39979523f21e26b69590db26
pasku <pasku@ubuntu.(none)>
2013-03-07 12:19:24 +0000

    incio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruby3::Application.config.secret_token = 'b5cf2f6f42fb8637a2854b142e09dcc1128042120b6819345db1496f02713528fd96d04d9f329b4d42594f6c00ed794eb948e972ba85d615a361a53ae748cd81'
